624d36f998da7ea553588889effb37f8d2197093
Match <tamiratteferi847@gmail.com>
2024-07-23 22:51:29 +0300

    Deleted CVFile.js in JS directory

--- JS/CVFile.js
+++
@@ -1,6 +0,0 @@
-// JavaScript function to dynamically update the current year
-document.addEventListener("DOMContentLoaded", function() {
-  var currentYearElement = document.getElementById("current-year");
-  var currentYear = new Date().getFullYear();
-  currentYearElement.textContent = currentYear;
-});
